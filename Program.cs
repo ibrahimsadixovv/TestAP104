51729613ce0d7ec47711115743fcf63411d13852
ibrahimsadixovv <116624182+ibrahimsadixovv@users.noreply.github.com>
2022-10-31 18:49:15 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+#region
+//int pow=0;
+//for (int i = 19; i >= 1; i/=2)
+//    if (i>1)
+//    {
+//        pow++;
+//    }
+//{
+//    Console.WriteLine(pow);
+//}
+#endregion
+
+#region
+//{
+//    int n = 128790903;
+//    int m = 0;
+//    int sum = 0;
+//    while (n > 0)
+//    {
+//        m = n % 10;
+//        sum = sum + m;
+//        n = n / 10;
+//    }
+//    Console.WriteLine(sum);
+//}
+#endregion
+
+#region
+int num = 17;
+if (num == 0 || num == 1)
+{
+    Console.WriteLine(num + " ne sade ne de murekkeb ededdir");
+}
+else
+{
+    for (int a = 2; a <= num / 2; a++)
+    {
+        if (num % a == 0)
+        {
+            Console.WriteLine(num + " murekkeb ededdir");
+
+        }
+
+    }
+    Console.WriteLine(num + " sade ededdir");
+}
+#endregion
---
+++ task1.cs
@@ -0,0 +1,48 @@
+﻿//TAPSIRIQ 1
+#region
+//int pow=0;
+//for (int i = 19; i >= 1; i/=2)
+//    if (i>1)
+//    {
+//        pow++;
+//    }
+//{
+//    Console.WriteLine(pow);
+//}
+#endregion
+//TAPSIRIQ 2
+#region
+//{
+//    int n = 128790903;
+//    int m = 0;
+//    int sum = 0;
+//    while (n > 0)
+//    {
+//        m = n % 10;
+//        sum = sum + m;
+//        n = n / 10;
+//    }
+//    Console.WriteLine(sum);
+//}
+#endregion
+//TAPSIRIQ 3
+#region
+int num1 = 17;
+if (num1 == 0 || num1 == 1)
+{
+    Console.WriteLine(num1 + " ne sade ne de murekkeb ededdir");
+}
+else
+{
+    for (int a = 2; a <= num1 / 2; a++)
+    {
+        if (num1 % a == 0)
+       {
+            Console.WriteLine(num1 + " murekkeb ededdir");
+
+      }
+
+   }
+Console.WriteLine(num1 + " sade ededdir");
+}
+#endregion
